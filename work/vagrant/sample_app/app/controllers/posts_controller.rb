f08aa8f1e21eeaef43c305eddfac7648ad69a77e
aikawa321 <42262654+aikawa321@users.noreply.github.com>
2018-10-01 20:14:35 +0900

    [Finish]2-7

--- work/vagrant/sample_app/app/controllers/posts_controller.rb
+++ work/vagrant/sample_app/app/controllers/posts_controller.rb
@@ -4,15 +4,11 @@ class PostsController < ApplicationController
   	@post = Post.new
   end
 
-  #以下を追加
- 	def create
- 		#ストロングパラメーターを使用
- 		 post=Post.new(post_params)
- 		#DBへ保存する
- 		 post.save
- 		#トップ画面へリダイレクト
- 		 redirect_to '/top'
- 	end
+  	def create
+		post = Post.new(post_params)
+		post.save # DBへ保存する
+		redirect_to post_path(post.id) # 詳細画面へリダイレクト
+	end
 
  	def index
  		@posts = Post.all
@@ -21,11 +17,14 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+	def edit
+		@post = Post.find(params[:id])
+	end
 
-	def create
-		post = Post.new(post_params)
-		post.save # DBへ保存する
-		redirect_to post_path(post.id) # 詳細画面へリダイレクト
+	def update
+		post = Post.find(params[:id])
+		post.update(post_params)
+		redirect_to post_path(post.id)
 	end
 
   	private
