778e0c413ba1fbc96a3ab0b3f59ea8556c60e763
aikawa321 <42262654+aikawa321@users.noreply.github.com>
2018-10-01 21:34:12 +0900

    [Finish]2-8

--- work/vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ work/vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cef945f534bc10c2c6c6f82fd9941268d90b1956549417ab0fef9f18c857bcdd65c0248c079203f297bdd8bedfe1f2b4f210d92fce971788c941cbd9c8151d63'
